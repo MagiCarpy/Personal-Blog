ba211ddbf3b227b8fe2dbfd960b062d61acf7696
MagiCarpy <49000672+MagiCarpy@users.noreply.github.com>
2024-10-30 08:04:38 -0700

    fixed navBar and made screen fit correctly

--- scripts.js
+++ scripts.js
@@ -7,22 +7,29 @@ function openCloseBar() {
     if (screen.style.minWidth != "0px") {
         showContent(false, screen);
         document.getElementById('sideBarBtn').innerHTML = "&#187;"
+        document.getElementById('contentBody').style.overflow = "hidden";
     } else {
         showContent(true, screen);
         document.getElementById('sideBarBtn').innerHTML = "&#171;"
+        document.getElementById('contentBody').style.overflow = "auto";
     }
 }
 
 function showContent(boolean, scrn) {
+    let contentHeader = document.getElementById('contentHeader');
+    let myContent = document.getElementById('myContent');
     if (boolean) {
-        document.getElementById('myContent').style.opacity = "1";
-        document.getElementById('contentHeader').style.opacity = "1";
+        contentHeader.style.opacity = "1";
+        myContent.style.opacity = "1";
         scrn.style.width = "200px";
         scrn.style.minWidth = "200px";
+        myContent.style.pointerEvents = "auto";
     } else {
-        document.getElementById('myContent').style.opacity = "0";
-        document.getElementById('contentHeader').style.opacity = "0";
+        contentHeader.style.opacity = "0";
+        myContent.style.opacity = "0";
         scrn.style.minWidth = "0px";
         scrn.style.width = "35px";
+        myContent.style.pointerEvents = "none";
+        
     }
 }
